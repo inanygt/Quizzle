2e8d635777ef430d6ec89819fcf889d34c394a47
inanygt <inanygt@hotmail.com>
2023-06-07 18:35:22 +0200

    added guard behind quizzle

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/quizzle', [QuizzleController::class, 'index'])->name('quizzle');
+Route::get('/quizzle', [QuizzleController::class, 'index'])->name('quizzle')->middleware('auth');
 Route::post('/quizzle', [QuizzleController::class, 'form']);
 
 
