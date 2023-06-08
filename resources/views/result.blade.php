863a3b98313fb2b83c988415ddd1572f245cc64b
JORIAN KASPRZAK <joriankasprzak@hotmail.com>
2023-06-08 16:34:56 +0200

    addded redirect button to make new quiz at results

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -11,6 +11,8 @@
         <h1>Quiz Results</h1>
         <h2>Your Score: {{ $score }}/{{ $quiz->num_questions }}</h2>
         <p>Thanks for participating in the quiz! You scored {{ $score }} out of {{ $quiz->num_questions }}.</p>
+
+        <a href="/quiz/start" class="btn">Start New Quiz</a>
     </div>
 </body>
 </html>
