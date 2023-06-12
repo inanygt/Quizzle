99bafe0a4205f615f8bf971ca471320f97f580ef
inanygt <inanygt@hotmail.com>
2023-06-12 10:51:08 +0200

    added redesign homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,14 +1,13 @@
 <link rel="stylesheet" href="{{ asset('css/homepage.css') }}">
 
-
-
-<div class="row row-front">
-    <div class="col">
-        <h1 class="homeHeading">Quizzle, Learn, Grow</h1>
-        <p>Unleash Your Knowledge!</p>
-    </div>
-    {{-- Buttons --}}
-    <div class="col">
-
+<div class="row row-front align-items-center justify-content-center">
+    <div class="col text-center">
+        <div class="hero">
+            <h1 class="homeHeading">Quizzle, Learn & Grow</h1>
+            <p class="deopacity">Unleash Your Knowledge!</p>
+            {{-- Buttons --}}
+            <a href="{{ route('quizzle') }}" class="btn btn-dark mx-2 deopacity"><i class="fa-solid pe-1 fa-circle-plus"></i> quizzle</a>
+            <a href="{{ route('quizzle') }}" class="btn btn-dark mx-2 deopacity"><i class="fa-regular pe-1 fa-circle-play"></i> random </a>
+        </div>
     </div>
 </div>
