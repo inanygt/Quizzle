59d62437c2c09e46e4355fd9502f2ef4584c9e00
inanygt <inanygt@hotmail.com>
2023-06-16 13:03:07 +0200

    result page redesign

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -1,10 +1,14 @@
 @extends('quizmaster')
 @section('content')
-    <div class="container">
-        <h1>Quiz Results</h1>
-        <h2>Your Score: {{ $score }}/{{ $quiz->num_questions }}</h2>
-        <p>Thanks for participating in the quiz! You scored {{ $score }} out of {{ $quiz->num_questions }}.</p>
+    <div class="vertical-center">
+        <div class="row">
+            <div class="col d-flex flex-column align-items-center text-center">
+            {{-- <h1>Quiz Results</h1> --}}
+            <h2>Your Score: {{ $score }}/{{ $quiz->num_questions }}</h2>
+            <p>Thanks for participating in the quiz! You scored {{ $score }} out of {{ $quiz->num_questions }}.</p>
 
-        <a href="/quiz/start" class="btn">Start New Quiz</a>
+            <a href="/quiz/start" class="btn btn-dark">Start New Quiz</a>
+            </div>
+        </div>
     </div>
 @endsection
